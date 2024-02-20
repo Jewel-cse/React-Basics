266a7d7ac9a8a8a4eea1a737fc3e8cd7a5410345
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-20 15:01:44 +0600

    use props to pass properties into component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './Counter.css';
 
-export default function Counter() {
+export default function Counter({by}) {  //by is an object
   //[0,f]
   const [count, setCount] = useState(0); //use state return an array of current value and function
 
   function increamentCounter() {
-    setCount(count + 1);
+    setCount(count + by);
   }
 
   function decreamentCounter() {
-    setCount(count - 1);
+    setCount(count - by);
   }
 
   return (
@@ -18,10 +18,10 @@ export default function Counter() {
       <span className="count">{count}</span>
       <div>
         <button className="counterButton" onClick={increamentCounter}>
-          +1
+          +{by}
         </button>
         <button className="counterButton" onClick={decreamentCounter}>
-          -1
+          -{by}
         </button>
       </div>
     </div>
