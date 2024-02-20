d95884c6220df761529fe2e28e0a87dc76d1e1f4
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-20 14:06:22 +0600

    add state to our component

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,17 +1,24 @@
-
-import './Counter.css'
+import { useState } from 'react';
+import './Counter.css';
 
 export default function Counter() {
+  //[0,f]
+  const [count, setCount] = useState(0); //use state return an array of current value and function
 
-    function increamentCounterFunction() {
-        console.log('increament clicked')
-    }
+  function increamentCounterFunction() {
+    setCount(count + 1);
+    //console.log(count)
+    //console.log('increament clicked')
+  }
 
-
-    return <div className="Counter">
-        <span className="count">0</span>
-        <div>
-            <button className="counterButton" onClick={increamentCounterFunction}>+1</button>
-        </div>
-  </div>;
+  return (
+    <div className="Counter">
+      <span className="count">{count}</span>
+      <div>
+        <button className="counterButton" onClick={increamentCounterFunction}>
+          +1
+        </button>
+      </div>
+    </div>
+  );
 }
