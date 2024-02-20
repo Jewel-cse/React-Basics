d307ed2b5c8f3e66a964b096ab2956e9a949e511
Jewel-cse <md.jewel.cse@gmail.com>
2024-02-20 14:37:50 +0600

    add decreament function

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -5,18 +5,23 @@ export default function Counter() {
   //[0,f]
   const [count, setCount] = useState(0); //use state return an array of current value and function
 
-  function increamentCounterFunction() {
+  function increamentCounter() {
     setCount(count + 1);
-    //console.log(count)
-    //console.log('increament clicked')
+  }
+
+  function decreamentCounter() {
+    setCount(count - 1);
   }
 
   return (
     <div className="Counter">
       <span className="count">{count}</span>
       <div>
-        <button className="counterButton" onClick={increamentCounterFunction}>
+        <button className="counterButton" onClick={increamentCounter}>
           +1
+        </button>
+        <button className="counterButton" onClick={decreamentCounter}>
+          -1
         </button>
       </div>
     </div>
